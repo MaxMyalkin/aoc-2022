499e5205677ddce6d519a86b5cf7989791c784a7
Максим Мялкин <maxmyalkin@gmail.com>
2022-12-06 02:23:12 +0300

    Complete Day5 task2

--- src/main/kotlin/day_5/Day5.kt
+++ src/main/kotlin/day_5/Day5.kt
@@ -34,6 +34,9 @@ private data class Movement(
     val from: Int,
     val to: Int
 )
+
+typealias MoveOperation = (fromStack: ArrayDeque<String>, toStack: ArrayDeque<String>, count: Int) -> Unit
+
 private fun readMovements(file: File): List<Movement> {
     return file.bufferedReader()
         .lineSequence()
@@ -43,39 +46,61 @@ private fun readMovements(file: File): List<Movement> {
         .toList()
 }
 
-private fun List<ArrayDeque<String>>.applyMovements(movements: List<Movement>) {
+private fun List<ArrayDeque<String>>.applyMovements(
+    movements: List<Movement>,
+    moveOperation: MoveOperation
+) {
     movements.forEach { movement ->
         val fromIndex = movement.from.dec()
         val toIndex = movement.to.dec()
-        (0 until movement.count).forEach {
-            val letter = this[fromIndex].removeLastOrNull() ?: error("Incorrect operation")
-            this[toIndex].addLast(letter)
-        }
+        moveOperation(this[fromIndex], this[toIndex], movement.count)
     }
+}
+
+private fun reverseOrderMove(fromStack: ArrayDeque<String>, toStack: ArrayDeque<String>, count: Int) {
+    (0 until count).forEach {
+        val letter = fromStack.removeLastOrNull() ?: error("Incorrect operation")
+        toStack.addLast(letter)
+    }
+}
+
+private fun defaultOrderMove(fromStack: ArrayDeque<String>, toStack: ArrayDeque<String>, count: Int) {
+    val lastCrates = fromStack.takeLast(count)
+    val remainingCrates = fromStack.dropLast(count)
+    fromStack.clear()
+    fromStack.addAll(remainingCrates)
+    toStack.addAll(lastCrates)
 }
 
 private fun List<ArrayDeque<String>>.getTopLetters(): String {
     return mapNotNull { it.lastOrNull() }
         .joinToString(separator = "")
 }
-private fun part1(file: File): String {
+
+private fun moveCrates(
+    file: File,
+    moveOperation: MoveOperation
+): String {
     val columnCount = readColumnCount(file)
-    if(columnCount <= 0) error("Check inputs")
+    if (columnCount <= 0) error("Check inputs")
 
     val stacks = List<ArrayDeque<String>>(columnCount) { ArrayDeque() }
     fillStacks(file, stacks)
 
     val movements = readMovements(file)
-    stacks.applyMovements(movements)
+    stacks.applyMovements(movements, moveOperation)
 
     return stacks.getTopLetters()
 }
+
 fun main() {
     val sample = getSampleFile(5)
     val input = getInputFile(5)
 
-    check(part1(sample) == "CMZ")
-    println("Part1 = ${part1(input)}")
+    check(moveCrates(sample, ::reverseOrderMove) == "CMZ")
+    println("Part1 = ${moveCrates(input, ::reverseOrderMove)}")
 
+    check(moveCrates(sample, ::defaultOrderMove) == "MCD")
+    println("Part2 = ${moveCrates(input, ::defaultOrderMove)}")
 
 }
